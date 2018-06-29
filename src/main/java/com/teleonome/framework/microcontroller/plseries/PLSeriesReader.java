6e545a5ca824ec2dff0dad4652bf441f4a696b19
Ari Fainchtein <arifainchtein@Aris-MacBook-Pro.local>
2018-06-29 15:14:54 +1000

    gg

--- src/main/java/com/teleonome/framework/microcontroller/plseries/PLSeriesReader.java
+++ src/main/java/com/teleonome/framework/microcontroller/plseries/PLSeriesReader.java
@@ -398,10 +398,17 @@ public class PLSeriesReader extends BufferedReader {
 			
 			
 			byte[] buffer = new byte[1024];
-			////logger.debug("point 3c");
-			serialPortInputStream.read(buffer);
+			logger.debug("about to read new wy");
+			//serialPortInputStream.read(buffer);
+			
+			
+			int readCount = readInputStreamWithTimeout(serialPortInputStream, buffer, 6000);  // 6 second timeout
+			logger.debug("readCount=" + readCount);
+		
+			
+			
 			int responseCode = convertByteToInt(buffer);
-			////logger.debug("getCurrentLoad responseCode:" + responseCode);
+			logger.debug("getCurrentLoad responseCode:" + responseCode);
         	
 			
 			double loadCurrent=0;
@@ -726,6 +733,20 @@ public class PLSeriesReader extends BufferedReader {
 	return 0;
 	}
 
+	
+	public static int readInputStreamWithTimeout(InputStream is, byte[] b, int timeoutMillis)
+		     throws IOException  {
+		     int bufferOffset = 0;
+		     long maxTimeMillis = System.currentTimeMillis() + timeoutMillis;
+		     while (System.currentTimeMillis() < maxTimeMillis && bufferOffset < b.length) {
+		         int readLength = java.lang.Math.min(is.available(),b.length-bufferOffset);
+		         // can alternatively use bufferedReader, guarded by isReady():
+		         int readResult = is.read(b, bufferOffset, readLength);
+		         if (readResult == -1) break;
+		         bufferOffset += readResult;
+		     }
+		     return bufferOffset;
+		 }
 	
 	public static double round(double value, int places) {
 	    if (places < 0) throw new IllegalArgumentException();
