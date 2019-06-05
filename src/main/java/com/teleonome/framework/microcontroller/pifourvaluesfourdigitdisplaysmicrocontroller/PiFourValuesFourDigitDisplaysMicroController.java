4cdad6ca702e98c86b9ddc2414ed4d8fdd786a20
Ari Fainchtein <ari@digitalgeppetto.com>
2019-06-06 07:01:46 +1000

    change the fourdisplay to use leds

--- src/main/java/com/teleonome/framework/microcontroller/pifourvaluesfourdigitdisplaysmicrocontroller/PiFourValuesFourDigitDisplaysMicroController.java
+++ src/main/java/com/teleonome/framework/microcontroller/pifourvaluesfourdigitdisplaysmicrocontroller/PiFourValuesFourDigitDisplaysMicroController.java
@@ -10,6 +10,10 @@ import java.util.ArrayList;
 import org.apache.log4j.Logger;
 import org.json.JSONArray;
 
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.RaspiPin;
 import com.pi4j.wiringpi.Gpio;
 import com.pi4j.wiringpi.SoftPwm;
 import com.teleonome.framework.LifeCycleEventListener;
@@ -27,7 +31,10 @@ public class PiFourValuesFourDigitDisplaysMicroController  extends MicroControll
 	private static int PIN_NUMBER_BLUE = 18;
 	 private static int colors[] = {  0x00FF00, 0x0000FF };
 
-
+	 final GpioController gpio = GpioFactory.getInstance();
+	 final GpioPinDigitalOutput synchronousCyclePin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_17);
+	 final GpioPinDigitalOutput aSynchronousCyclePin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_18);
+	    
 	PiFourValuesFourDigitDisplaysWriter aPiFourValuesFourDigitDisplaysWriter;
 	PiFourValuesFourDigitDisplaysReader aPiFourValuesFourDigitDisplaysReader;
 	StringWriter sw = new StringWriter();
@@ -54,11 +61,16 @@ public class PiFourValuesFourDigitDisplaysMicroController  extends MicroControll
 		aPiFourValuesFourDigitDisplaysWriter = new PiFourValuesFourDigitDisplaysWriter(sw, configParams, aDenomeManager) ;
 		aPiFourValuesFourDigitDisplaysReader = new PiFourValuesFourDigitDisplaysReader(new StringReader(""), aPiFourValuesFourDigitDisplaysWriter) ;
 
-		Gpio.wiringPiSetup();
+		
 
-		SoftPwm.softPwmCreate(PIN_NUMBER_RED, 0, 100);
-		SoftPwm.softPwmCreate(PIN_NUMBER_GREEN, 0, 100);
-		SoftPwm.softPwmCreate(PIN_NUMBER_BLUE, 0, 100);
+	   
+		
+
+	    
+//		Gpio.wiringPiSetup();
+//		SoftPwm.softPwmCreate(PIN_NUMBER_RED, 0, 100);
+//		SoftPwm.softPwmCreate(PIN_NUMBER_GREEN, 0, 100);
+//		SoftPwm.softPwmCreate(PIN_NUMBER_BLUE, 0, 100);
 
 		logger.info(" PiFourValuesFourDigitDisplaysMicroController  " );
 
@@ -85,8 +97,8 @@ public class PiFourValuesFourDigitDisplaysMicroController  extends MicroControll
 		logger.debug("processing life cycle even=t=" + lifeCycleEvent);
 		if(lifeCycleEvent.equals(TeleonomeConstants.LIFE_CYCLE_EVENT_START_SYNCHRONOUS_CYCLE)) {
 			//String command = "neouart -i 00ff0000 00ff0000";
-			
-			ledColorSet(0x00FF00);
+			synchronousCyclePin.toggle();
+			//ledColorSet(0x00FF00);
 //			try {
 //				ArrayList<String> results  = Utils.executeCommand(command);
 //				logger.debug("processing life cycle event start pulse produced=" + results);
@@ -96,7 +108,8 @@ public class PiFourValuesFourDigitDisplaysMicroController  extends MicroControll
 //			}
 
 		}else if(lifeCycleEvent.equals(TeleonomeConstants.LIFE_CYCLE_EVENT_END_SYNCHRONOUS_CYCLE)) {
-			ledColorSet(0x00FF00);
+			synchronousCyclePin.toggle();
+			//ledColorSet(0x00FF00);
 //			String command = "neouart -i 0000ff00 0000ff00";
 //			try {
 //				ArrayList<String> results  = Utils.executeCommand(command);
@@ -106,6 +119,7 @@ public class PiFourValuesFourDigitDisplaysMicroController  extends MicroControll
 //				logger.warn(Utils.getStringException(e));
 //			}
 		}else if(lifeCycleEvent.equals(TeleonomeConstants.LIFE_CYCLE_EVENT_START_ASYNCHRONOUS_CYCLE)) {
+			aSynchronousCyclePin.toggle();
 //			String command = "neouart -i 0000ff00 0000ff00";
 //			try {
 //				ArrayList<String> results  = Utils.executeCommand(command);
@@ -116,6 +130,7 @@ public class PiFourValuesFourDigitDisplaysMicroController  extends MicroControll
 //			}
 			ledColorSet(0x000FF);
 		}else if(lifeCycleEvent.equals(TeleonomeConstants.LIFE_CYCLE_EVENT_END_ASYNCHRONOUS_CYCLE)) {
+			aSynchronousCyclePin.toggle();
 //			String command = "neouart -i 00ff0000 00ff0000";
 //			try {
 //				ArrayList<String> results  = Utils.executeCommand(command);
@@ -124,27 +139,27 @@ public class PiFourValuesFourDigitDisplaysMicroController  extends MicroControll
 //				// TODO Auto-generated catch block
 //				logger.warn(Utils.getStringException(e));
 //			}
-			ledColorSet(0x000FF);
+			//ledColorSet(0x000FF);
 		}
 	}
 
-	private int map(int x, int in_min, int in_max, int out_min, int out_max) {
-		return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-	}
-
-	private void ledColorSet(int color) {
-		int r_val, g_val, b_val;
-
-		r_val = (color & 0xFF0000) >> 16; // get red value
-		g_val = (color & 0x00FF00) >> 8; // get green value
-		b_val = (color & 0x0000FF) >> 0; // get blue value
-
-		r_val = map(r_val, 0, 255, 0, 100); // change a num(0~255) to 0~100
-		g_val = map(g_val, 0, 255, 0, 100);
-		b_val = map(b_val, 0, 255, 0, 100);
-
-		SoftPwm.softPwmWrite(PIN_NUMBER_RED, 100 - r_val); // change duty cycle
-		SoftPwm.softPwmWrite(PIN_NUMBER_GREEN, 100 - g_val);
-		SoftPwm.softPwmWrite(PIN_NUMBER_BLUE, 100 - b_val);
-	}
+//	private int map(int x, int in_min, int in_max, int out_min, int out_max) {
+//		return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+//	}
+//
+//	private void ledColorSet(int color) {
+//		int r_val, g_val, b_val;
+//
+//		r_val = (color & 0xFF0000) >> 16; // get red value
+//		g_val = (color & 0x00FF00) >> 8; // get green value
+//		b_val = (color & 0x0000FF) >> 0; // get blue value
+//
+//		r_val = map(r_val, 0, 255, 0, 100); // change a num(0~255) to 0~100
+//		g_val = map(g_val, 0, 255, 0, 100);
+//		b_val = map(b_val, 0, 255, 0, 100);
+//
+//		SoftPwm.softPwmWrite(PIN_NUMBER_RED, 100 - r_val); // change duty cycle
+//		SoftPwm.softPwmWrite(PIN_NUMBER_GREEN, 100 - g_val);
+//		SoftPwm.softPwmWrite(PIN_NUMBER_BLUE, 100 - b_val);
+//	}
 }
