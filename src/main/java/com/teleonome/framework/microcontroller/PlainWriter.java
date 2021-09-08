6710dfb138a3a817039f2e6e6c730e3a64a9fb1a
arifainchtein <ari@digitalgeppetto.com>
2021-09-08 16:20:57 +1000

    fixing mnemosycons

--- src/main/java/com/teleonome/framework/microcontroller/PlainWriter.java
+++ src/main/java/com/teleonome/framework/microcontroller/PlainWriter.java
@@ -4,16 +4,20 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.Writer;
 
-public class PlainWriter extends BufferedWriter{
+import org.apache.log4j.Logger;
 
+public class PlainWriter extends BufferedWriter{
+	Logger logger;
 	PlainReader plainReader;
 	
 	public PlainWriter(Writer out, PlainReader p) {
 		super(out);
+		logger = Logger.getLogger(getClass());
 		plainReader=p;
 	}
 	
 	public void write(String command, int off, int len) throws IOException {
+		logger.debug("Get command " + command);
 		plainReader.setCurrentCommand(command);
 	}
 }
