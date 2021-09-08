b62134a92560d6ecf8b2c1e64dea1a30b6ca37fa
arifainchtein <ari@digitalgeppetto.com>
2021-09-08 10:09:04 +1000

    fixing mnemosycons

--- src/main/java/com/teleonome/framework/mnemosyne/MnemosyneManager.java
+++ src/main/java/com/teleonome/framework/mnemosyne/MnemosyneManager.java
@@ -494,9 +494,11 @@ public class MnemosyneManager {
 					rowsDeleted = aDBManager.deleteByPeriodFromOrganismPulse(millisToDeleteFrom, teamParam);
 					logger.debug("rowsDeleted=" + rowsDeleted);
 				}	
-
+				logger.debug("line 497 about to vacum");
+				  now = System.currentTimeMillis();
 				aDBManager.vacuum();
-
+				long vacumingTime = System.currentTimeMillis()-now;
+				logger.debug("line 500  vacumg took " + vacumingTime);
 				mnemosyconRuleProcessingDeneWord = Utils.createDeneWordJSONObject("Rows Deleted", rowsDeleted,null,"int",true);
 				mnemosyconRuleProcessingDeneDeneWords.put(mnemosyconRuleProcessingDeneWord);
 
