98be0687065f3bafe64214479d2b4c089c7cf246
Ari Fainchtein <ari@digitalstables.com>
2025-09-30 22:11:17 +1000

    fixing issues

--- src/main/java/com/teleonome/framework/microcontroller/annabellemicrocontroller/AnnabelleReader.java
+++ src/main/java/com/teleonome/framework/microcontroller/annabellemicrocontroller/AnnabelleReader.java
@@ -100,7 +100,6 @@ public class AnnabelleReader extends BufferedReader{
 							deserializer="SeedlingMonitorDataDeserializer";
 							processString=true;
 							appendString=false;
-							}
 						}
 					}
 					
